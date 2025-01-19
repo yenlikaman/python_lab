3bb60d74ce773f20a1698b66684eb5c9f3caf2ae
yenlikaman <y_aman@kbtukz>
2025-01-19 23:55:52 +0500

    Concatenation Format - Strings

---
+++ Concatenation-FormatStrings.py
@@ -0,0 +1,24 @@
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
