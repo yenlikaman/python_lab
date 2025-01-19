97fb203a6342ed0d19fd8ea0d1fc08d241cb63c9
yenlikaman <y_aman@kbtukz>
2025-01-19 23:51:22 +0500

    Slicing-Modify

---
+++ Slicing-Modify.py
@@ -0,0 +1,27 @@
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
